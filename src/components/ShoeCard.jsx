b2eeef42c07e837e06cfe0d3cfde808d804cb187
yuuki <kakashi16023@gmail.com>
2024-03-26 21:44:59 +0700

    add: add feature Popular  products

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -16,12 +16,12 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
       } cursor-pointer max-sm:flex-1`}
       onClick={handelClick}
     >
-      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-40">
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
         <img
           src={imgURL.thumbnail}
           alt="shoe collection"
           width={127}
-          height={103}
+          height={103.34}
           className="object-contain"
         />
       </div>
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
         </p>
-        <h1 className="mt10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
@@ -30,7 +30,7 @@ const Hero = () => {
         <Button label="Shop now" iconURL={arrowRight} />
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat, index) => (
-            <div key={stat.label}>
+            <div key={index}>
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stat.label}
